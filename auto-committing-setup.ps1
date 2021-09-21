66ec7c68edd76d57d2a011f592ee929b50314d80
Mike <mike.sierra.7950@gmail.com>
2021-09-21 12:33:54 -0400

    Auto committed from auto-committing-setup.ps1!

---
+++ auto-committing-setup.ps1
@@ -0,0 +1,23 @@
+#declare variables
+$GitHubUsername='Sanchez2047'
+$PracticeRepoDir='$(pwd)\powershell-practice'
+$CommitMessage='Auto committed from auto-committing-setup.ps1!'
+
+#fork and lcone this repo in the powershell-studio directory
+git clone "https://github.com/$GitHubUsername/powershell-practice" $PracticeRepoDir
+
+# 5. Copy the auto-committing-setup.ps1 script (by its absolute path) into the cloned repo directory
+cp $PSCommandPath $PracticeRepoDir
+
+# 6. Change into the cloned directory ($PracticeRepoDir)
+cd $PracticeRepoDir
+
+#7. Add the new script file in the cloned directory to git staging
+git add .
+
+#8. Commit the changes to the repo using the message variable ($CommitMessage)
+git commit -m "$CommitMessage"
+
+#9. Push the local git history back to your forked repo
+git push
+
